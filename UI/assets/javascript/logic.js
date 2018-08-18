12a388e0633c05fbb00566e2648a621ef513d042
jesberman <jesberman@gmail.com>
2018-08-18 13:06:36 -0400

    clinetSurveyStart

---
+++ UI/assets/javascript/logic.js
@@ -0,0 +1,16 @@
+$("#next1").on("click", function () {
+
+        $("#q1").css("display","none");
+
+
+});
+
+
+$("#next1").on("click", function () {
+
+    $("#q2").css("display", "block");
+
+
+});
+
+
