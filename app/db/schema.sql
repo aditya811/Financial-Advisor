438d8deea4237b70e1729b7216e02348b0d9b0fd
rfogari <fogari10@gmail.com>
2018-08-25 13:05:51 -0400

    Updated schema. Added seeds file.

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,28 +1,46 @@
-DROP database financial_advisor_db;
-create database financial_advisor_db;
-use financial_advisor_db;
+DROP DATABASE IF EXISTS financial_adviser_db;
+create database financial_adviser_db;
+use financial_adviser_db;
 
-CREATE TABLE clients
+CREATE TABLE adviser
 (
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(20) NOT NULL,
-	phone varchar(10) NOT NULL,
-    email varchar(55) NOT NULL,
-	PRIMARY KEY (id)
-);
+	id int auto_increment NOT NULL,
+	business_name TEXT NOT NULL,
+	office_address_1 TEXT NOT NULL,
+	office_address_2 TEXT,
+	office_city TEXT NOT NULL,
+	office_state TEXT NOT NULL,
+	office_zipcode TEXT NOT NULL,
+	office_telephone TEXT NOT NULL,
+	office_website TEXT,
+    adviser_name TEXT NOT NULL,
+    adviser_email TEXT NOT NULL,
+    adviser_id int NOT NULL,
+    adviser_service TEXT NOT NULL,
+    client_bracket TEXT NOT NULL,
+    key(id),
+    primary key(adviser_id)
+    )ENGINE=InnoDB;
 
-CREATE TABLE advisors
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(20) NOT NULL,
-	phone varchar(10) NOT NULL,
-    email varchar(55) NOT NULL,
-	PRIMARY KEY (id)
-);
 
-CREATE TABLE advisors_search
+CREATE TABLE client
 (
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(20) NOT NULL,
-	PRIMARY KEY (id)
-);
+	client_id int auto_increment NOT NULL,
+    client_name TEXT NOT NULL,
+    client_email TEXT NOT NULL,
+    client_phone TEXT NOT NULL,
+    client_zipcode TEXT NOT NULL,
+    client_retirement TEXT NOT NULL,
+    client_currently_manage TEXT NOT NULL,
+    client_has_children TEXT NOT NULL,
+    client_owns_business TEXT NOT NULL,
+    client_advisor_tax_specialty TEXT NOT NULL,
+    client_advisor_estate_specialty TEXT NOT NULL,
+    client_existing_advisor TEXT NOT NULL,
+    client_advisor_immediate_area TEXT NOT NULL,
+    client_household_income TEXT NOT NULL,
+    client_investable_assets TEXT NOT NULL,
+    client_monthly_savings TEXT NOT NULL,
+    client_additional_data TEXT NOT NULL,
+    primary key(client_id)
+)engine=InnoDB
--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -1,27 +1,37 @@
-var db = require("../models");
+var path = require('path');
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+
+
+  //home route
+
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  //about route
+  app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/about.html'));
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  // contact route
+  app.get('/contact', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/contact.html'));
   });
+
+  //client survey questions route
+  app.get('/client', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/client-survey-html'));
+  });
+
+  //client survey financial form route
+  app.get('/advisor', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/profile-fill-out.html'));
+  })
+
+
 };
+
+
+
+
